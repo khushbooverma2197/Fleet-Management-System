0441eff83b7f7532b0778fd2dcf8b544af4350aa
khushbooverma2197 <khushbooverma2197@gmail.com>
2026-01-31 20:09:19 +0530

    Update user.routes.js

--- root/routes/user.routes.js
+++ root/routes/user.routes.js
@@ -1,7 +1,9 @@
-const express = require('express');
-const { signup } = require('../controllers/user.controller');
+const express = require("express");
+const { signup, getAllUsers, getAnalytics } = require("../controllers/user.controller");
 const router = express.Router();
 
-router.post('/signup', signup);
+router.post("/signup", signup);
+router.get("/", getAllUsers);
+router.get("/analytics", getAnalytics);
 
 module.exports = router;
