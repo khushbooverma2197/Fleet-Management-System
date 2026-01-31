a259986f2ccd2613abf634efc862d7664ad2df78
khushbooverma2197 <khushbooverma2197@gmail.com>
2026-01-31 20:05:09 +0530

    Update supabase.js

--- root/config/supabase.js
+++ root/config/supabase.js
@@ -1,7 +1,11 @@
-const { createClient } = require('@supabase/supabase-js');
+const { createClient } = require("@supabase/supabase-js");
+
+// Disable certificate validation for development
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY
+  process.env.SUPABASE_KEY
 );
+
 module.exports = supabase;
